6aaf2e23288f604853611c3bc519f519371fb796
Anton Pegov <antonpegov@gmail.com>
2015-05-04 23:47:59 +0000

    .

--- api/config.js
+++ api/config.js
@@ -2,5 +2,6 @@ module.exports = {
 
     userSecret : 'some words...',
     adminSecret: 'Putin - Huilo, la-la-la-la-la-la-la-la!',
-    db: 'mongodb://localhost/happy'
+    db: 'mongodb://localhost/happy',
+    port: 8080
 };
