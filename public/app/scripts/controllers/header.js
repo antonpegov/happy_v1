b89f8ae6d7af1ece348a80457b1258ceec70611b
Anton Pegov <antonpegov@gmail.com>
2015-05-17 15:31:35 +0000

    Адаптировал стили для смартфонов (до 480 пикселей)

--- public/app/scripts/controllers/header.js
+++ public/app/scripts/controllers/header.js
@@ -10,4 +10,8 @@ angular.module('happyTurtlesApp')
   .controller('HeaderCtrl', function ($scope,authToken) {
     $scope.isAuthenticated = authToken.isAuthenticated;
     console.log('isAuthanticated()='+$scope.isAuthenticated());
+    $('.nav a').on('click', function() {
+          $(".btn-navbar").click(); //bootstrap 2.x
+          $(".navbar-toggle").click() //bootstrap 3.x by Richard
+    });
   });
