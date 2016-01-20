28336c7a89b91f7e536196139f2cbfd288742bd5
Anton Pegov <antonpegov@gmail.com>
2016-01-20 23:12:48 +0000

    config.js оказывается где-то потерялся. Настройки для OpenShift.

---
+++ api/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	ipaddress : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+	port      : process.env.OPENSHIFT_NODEJS_PORT || 3000,
+
+    userSecret : 'some words...',
+    adminSecret: 'Putin - Huilo, la-la-la-la-la-la-la-la!',
+    db: 'mongodb://localhost/happy'
+};
